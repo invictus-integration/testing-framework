40593fb9009a4904490549c5f23c07a62b79c75c
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-10-12 12:56:32 +0200

    pr-sug: use '...SecretName' suffix for 'ISecretProvider' secret names

--- src/Invictus.Testing.LogicApps/LogicAppAuthentication.cs
+++ src/Invictus.Testing.LogicApps/LogicAppAuthentication.cs
@@ -29,25 +29,25 @@ namespace Invictus.Testing.LogicApps
         /// <param name="tenantId">The ID where the resources are located on Azure.</param>
         /// <param name="subscriptionId">The ID that identifies the subscription on Azure.</param>
         /// <param name="clientId">The ID of the client or application that has access to the logic apps running on Azure.</param>
-        /// <param name="clientSecretKey">The secret of the client or application that has access to the logic apps running on Azure.</param>
-        /// <param name="secretProvider">The provider to get the client secret; using the <paramref name="clientSecretKey"/>.</param>
+        /// <param name="clientSecretName">The secret of the client or application that has access to the logic apps running on Azure.</param>
+        /// <param name="secretProvider">The provider to get the client secret; using the <paramref name="clientSecretName"/>.</param>
         /// <param name="cloud">The Azure cloud environment to use during authenticating and interacting with the Azure Logic Apps resources.</param>
-        /// <exception cref="ArgumentException">Thrown when the <paramref name="tenantId"/>, <paramref name="subscriptionId"/>, <paramref name="clientId"/>, or <paramref name="clientSecretKey"/> is blank.</exception>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="tenantId"/>, <paramref name="subscriptionId"/>, <paramref name="clientId"/>, or <paramref name="clientSecretName"/> is blank.</exception>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="secretProvider"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown when the <paramref name="cloud"/> is outside the bounds of the enumeration.</exception>
-        public static LogicAppAuthentication UsingServicePrincipal(string tenantId, string subscriptionId, string clientId, string clientSecretKey, ISecretProvider secretProvider, AzureCloud cloud = AzureCloud.Global)
+        public static LogicAppAuthentication UsingServicePrincipal(string tenantId, string subscriptionId, string clientId, string clientSecretName, ISecretProvider secretProvider, AzureCloud cloud = AzureCloud.Global)
         {
             Guard.NotNullOrWhitespace(tenantId, nameof(tenantId), "Requires an tenant ID where the Azure resources are located");
             Guard.NotNullOrWhitespace(subscriptionId, nameof(subscriptionId), "Requires an ID that identifies the Azure subscription that has access to the Azure resources");
             Guard.NotNullOrWhitespace(clientId, nameof(clientId), "Requires an client or application ID that is authorized to interact with the Logic Apps running on Azure");
-            Guard.NotNullOrWhitespace(clientSecretKey, nameof(clientSecretKey), "Requires an client or application secret key that points to the secret of the client or application that is authorized to interact with the Logic Apps running on Azure");
+            Guard.NotNullOrWhitespace(clientSecretName, nameof(clientSecretName), "Requires an client or application secret key that points to the secret of the client or application that is authorized to interact with the Logic Apps running on Azure");
             Guard.NotNull(secretProvider, nameof(secretProvider), "Requires an secret provider instance to retrieve the secret of the client or application that is authorized to interact with the Logic Apps running on Azure");
             Guard.For(() => !Enum.IsDefined(typeof(AzureCloud), cloud), 
                 new ArgumentOutOfRangeException(nameof(cloud), cloud, "Requires the Azure cloud environment to be within the bounds of the enumeration"));
 
             return new LogicAppAuthentication(async () =>
             {
-                string clientSecret = await secretProvider.GetRawSecretAsync(clientSecretKey);
+                string clientSecret = await secretProvider.GetRawSecretAsync(clientSecretName);
                 LogicManagementClient managementClient = await AuthenticateLogicAppsManagementAsync(subscriptionId, tenantId, clientId, clientSecret, cloud);
 
                 return managementClient;
@@ -99,19 +99,19 @@ namespace Invictus.Testing.LogicApps
         /// Uses an access token to authenticate with Azure.
         /// </summary>
         /// <param name="subscriptionId">The ID that identifies the subscription on Azure.</param>
-        /// <param name="accessTokenKey">The secret key to use to fetch access token from the secret provider. This will be used to call the Azure management API.</param>
-        /// <param name="secretProvider">The provider to get the client secret; using the <paramref name="accessTokenKey"/>.</param>
-        /// <exception cref="ArgumentException">Thrown when the <paramref name="subscriptionId"/> or <paramref name="accessTokenKey"/> is blank.</exception>
+        /// <param name="accessTokenSecretName">The secret key to use to fetch access token from the secret provider. This will be used to call the Azure management API.</param>
+        /// <param name="secretProvider">The provider to get the client secret; using the <paramref name="accessTokenSecretName"/>.</param>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="subscriptionId"/> or <paramref name="accessTokenSecretName"/> is blank.</exception>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="secretProvider"/> is blank.</exception>
-        public static LogicAppAuthentication UsingAccessToken(string subscriptionId, string accessTokenKey, ISecretProvider secretProvider)
+        public static LogicAppAuthentication UsingAccessToken(string subscriptionId, string accessTokenSecretName, ISecretProvider secretProvider)
         {
             Guard.NotNullOrWhitespace(subscriptionId, nameof(subscriptionId), "Requires an ID that identifies the Azure subscription that has access to the Azure resources");
-            Guard.NotNullOrWhitespace(accessTokenKey, nameof(accessTokenKey), "Requires an access token secret key that points to the access token of the client or application that is authorized to interact with the Logic Apps running on Azure");
+            Guard.NotNullOrWhitespace(accessTokenSecretName, nameof(accessTokenSecretName), "Requires an access token secret key that points to the access token of the client or application that is authorized to interact with the Logic Apps running on Azure");
             Guard.NotNull(secretProvider, nameof(secretProvider), "Requires an secret provider instance to retrieve the access token of the client or application that is authorized to interact with the Logic Apps running on Azure");
 
             return new LogicAppAuthentication(async () =>
             {
-                string accessToken = await secretProvider.GetRawSecretAsync(accessTokenKey);
+                string accessToken = await secretProvider.GetRawSecretAsync(accessTokenSecretName);
                 LogicManagementClient managementClient = AuthenticateLogicAppsManagement(subscriptionId, accessToken);
 
                 return managementClient;
